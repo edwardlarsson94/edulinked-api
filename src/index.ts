184a0af3662939faef698076e7cb7faea118a836
edward vega <edward.vega@wiedii.co>
2024-10-04 15:25:32 -0500

    0009 add route for post

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import authRouter from './routes/authRouter';
+import postRouter from './routes/postRouter';
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Use the auth router
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port localhost:${port}`)
